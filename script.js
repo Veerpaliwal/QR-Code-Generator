2bc80178053f94e8f587b2b586f10f612586dece
Veerpaliwal <veerpaliwal901gmail.com>
2026-03-31 15:46:40 +0530

    new

---
+++ script.js
@@ -0,0 +1,40 @@
+const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes')
+const generateBtn = document.getElementById('generateBtn')
+const downloadBtn = document.getElementById('downloadBtn')
+
+const qrBody = document.querySelector('.qr-body');
+
+let size = sizes.value;
+
+generateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+   isEmptyInput();
+});
+
+sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    isEmptyInput();
+    
+});
+
+function isEmptyInput() {
+     if (qrText.value.length > 0) {
+         generateQRCode(); 
+    }
+    else {
+        alert("Enter the text or URL to generate your QR code ")
+    } 
+}
+
+function generateQRCode() {
+    qrBody.innerHTML = "";
+    new QRCode(qrBody, {
+        text: qrText.value,
+        height: size,
+        width: size,
+        colorLigth: "#fff",
+        colorDark:"#000"
+        
+    })
+}
